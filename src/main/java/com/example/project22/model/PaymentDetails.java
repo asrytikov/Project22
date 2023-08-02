fc5d9ae2f51a5d3a01bd0768887814b29376ab82
asrytikov <asrytikov@gmail.com>
2023-08-02 20:36:16 +0400

    Project 22

--- src/main/java/com/example/project22/model/PaymentDetails.java
+++ src/main/java/com/example/project22/model/PaymentDetails.java
@@ -1,2 +1,16 @@
-package com.example.project22.model;public class PaymentDetails {
+package com.example.project22.model;
+
+public class PaymentDetails {
+
+
+    private double amount;
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
 }
